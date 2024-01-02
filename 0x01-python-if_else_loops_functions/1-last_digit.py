d394f6ccbfa0bf1ba2553daddaef92685a721309
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-02 12:00:30 -0800

    python loops, if else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
     lastdig = -(lastdig)
 Thestrg = "Last digit of {} is {}".format(number, lastdig)
 if lastdig > 5:
-    print(f"{Thestrg} and it is greater than 5.")
-elif lastdig == 0 :
+    print(f"{Thestrg} and is greater than 5.")
+elif lastdig == 0:
     print(f"{Thestrg} and is 0")
 elif lastdig < 6:
     print(f"{Thestrg} and is less than 6 and not 0")
