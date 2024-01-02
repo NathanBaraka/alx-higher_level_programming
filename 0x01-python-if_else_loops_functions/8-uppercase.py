ff5df94609fe3e936d08c716cfb8783b8256eb90
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-02 13:52:15 -0800

    python loops, if else

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for iterator in str:
-        temp = iterator
+    for i in str:
+        temp = i
         if ord(temp) >= 97 and ord(temp) <= 122:
-            temp + chr(ord(iterator) - 32)
+            temp = chr(ord(i) - 32)
         print("{}".format(temp), end='')
     print()
