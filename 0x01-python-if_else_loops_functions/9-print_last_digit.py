0dfbb71bc4d3edd8fa54d4baa54dee56ef3dd09b
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-02 14:01:09 -0800

    python loops, if else

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number <0:
+    if number < 0:
         number = -number
         lastdig = number % 10
         print("{}".format(lastdig), end='')
