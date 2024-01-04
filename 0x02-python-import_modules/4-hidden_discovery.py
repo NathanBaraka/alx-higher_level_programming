c6f0657bdf9b181527ae439768a1407bf463c8f9
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-04 12:27:59 -0800

    python imports

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     alf = dir()
     for i in range(0, len(alf)):
-        if alf[i] [:2] != "__":
+        if alf[i][:2] != "__":
             print("{:s}".format(alf[i]))
