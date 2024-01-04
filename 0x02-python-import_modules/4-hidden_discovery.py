fc7dd584498fdd28344dd7fd43e8a4a7e8821f3c
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-04 12:24:55 -0800

    python imports

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    alf = dir()
+    for i in range(0, len(alf)):
+        if alf[i] [:2] != "__":
+            print("{:s}".format(alf[i]))
