bc71cec25ac8e3a4acc0f72d9569853b28e0c608
NathanBaraka <nathanbaraka23@gmail.com>
2024-01-24 10:41:35 -0800

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""square class defination"""
+
+
+class Square:
+    """This is the square class"""
+
+    def __init__(self, size):
+        """New square initialization
+
+        Args:
+            size (integer): New square size
+        """
+        self.__size = size
+
