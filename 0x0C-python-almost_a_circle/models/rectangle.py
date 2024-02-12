b9f3b845f1cb12f743015a649844466d556470e3
NathanBaraka <nathanBaraka23@gmail.com>
2024-02-12 05:52:57 -0800

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,14 +96,3 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
-
