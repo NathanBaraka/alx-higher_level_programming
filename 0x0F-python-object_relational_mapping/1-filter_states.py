db80166f1c69d6a6d77a7c4a519b10559ec7db86
NathanBaraka <nathanbaraka23@gmail.com>
2024-04-14 23:18:32 -0700

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,14 @@
 #!/usr/bin/python3
-"""This module lists all states from the mySQL database"""
+"""This module lists all states from the hbtn_0e_0_usa database."""
 import sys
 import MySQLdb
 
-def list_states (username, password, database):
-    """The lists all the states from the database hbtn_0e_0_usa.
-    Ags:
-        username: mysql username
-        password: mysql password
-        database: mysql database
-    """
-    # Connecting to the MySQL server.
-    db = MySQLdb.connect(host='localhost',\
-            port=3306,\
-            user=username,\
-            passwd=password,\
-            db=database)
-    cursor = db.cursor()
+if __name__ == "__main__":
+    # Gets the MySQL credentials from command-line args.
+    # Connects to MySQL server.
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    # Executing the SQL query to fetch all states.
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetching all the rows from the query result
-    rows = cursor.fetchall()
-
-    # Printing the results.
-    for row in rows:
-        print(row)
-
-    # Closing the database connection.
-    db.close()
-
-# Example usage
-if __name__ == '__main__':
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
-
+    # Executing the SQL query to retrieve all states sorted by id.
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
