c8466ac0a9edd39a63302d40b47f585e53e06a1a
NathanBaraka <nathanbaraka23@gmail.com>
2024-05-30 18:30:35 +0000

    jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+// Toggling the class of HTML tag 'HEADER' when user clicks on the
+// div#toggle_header tag.
+
+$('div#toggle_header').click(function () {
+    $('header').ToggleClass('red');
+  });
